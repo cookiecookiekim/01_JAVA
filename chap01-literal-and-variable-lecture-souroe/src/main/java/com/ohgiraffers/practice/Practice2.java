8fd2d6c4d589787f314049861064ef81b1c8d2db
gyunam kim <kimkunam06@gmail.com>
2024-08-26 17:43:15 +0900

    [24-08-26] Practice 1,2

---
+++ chap01-literal-and-variable-lecture-souroe/src/main/java/com/ohgiraffers/practice/Practice2.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.practice;
+
+public class Practice2 {
+
+    public static void main(String[] args) {
+
+        double width = 12.5; // 너비
+        double height = 36.4; // 높이
+
+        System.out.println("면적 : " + (width * height));
+        System.out.println("둘레 : " + (width + width + height + height));
+
+    }
+}
