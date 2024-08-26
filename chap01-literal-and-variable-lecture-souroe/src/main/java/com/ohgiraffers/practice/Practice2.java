f18df15f43a2688066d052e154f3f73bbe18afe6
gyunam kim <kimkunam06@gmail.com>
2024-08-26 18:04:29 +0900

    [24-08-26] 두 번째 수정

--- chap01-literal-and-variable-lecture-souroe/src/main/java/com/ohgiraffers/practice/Practice2.java
+++ chap01-literal-and-variable-lecture-souroe/src/main/java/com/ohgiraffers/practice/Practice2.java
@@ -7,8 +7,11 @@ public class Practice2 {
         double width = 12.5; // 너비
         double height = 36.4; // 높이
 
-        System.out.println("면적 : " + (width * height));
-        System.out.println("둘레 : " + (width + width + height + height));
+        double area = width * height;
+        double round = 2 * (width + height);
+
+        System.out.println("면적 : " + area);
+        System.out.println("둘레 : " + round);
 
     }
 }
